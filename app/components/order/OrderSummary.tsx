4066edadf85c85f093a3cfd49b429a5f5b26171b
agustinsosa10 <118317303+agustinsosa10@users.noreply.github.com>
2025-04-16 21:19:16 -0300

    agregando componentes

---
+++ app/components/order/OrderSummary.tsx
@@ -0,0 +1,7 @@
+export default function OrderSummary() {
+  return (
+    <div className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
+      <h1 className="text-4xl text-center font-black"> Mi pedido</h1>
+    </div>
+  )
+}
